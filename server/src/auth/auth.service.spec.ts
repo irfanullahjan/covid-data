a01d9fe9fd1193967c5389955bf521a21f7a0cc4
Irfanullah Jan <irfanullah.jan@outlook.com>
2023-04-10 14:55:36 -0400

    auth service tests

--- server/src/auth/auth.service.spec.ts
+++ server/src/auth/auth.service.spec.ts
@@ -6,7 +6,9 @@ import { UserService } from '../user/user.service';
 describe('AuthService', () => {
   let service: AuthService;
 
-  const mockUserService = {};
+  const mockUserService = {
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,5 +23,44 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findUser', () => {
+    it('should call userService.findOne', async () => {
+      const email = 'abc@example.com';
+      service.getUserById(email);
+      expect(mockUserService.findOne).toBeCalledWith(email);
+    });
+
+    it('should return undefined if no email is provided', async () => {
+      const email = undefined;
+      const result = service.getUserById(email);
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('validateUser', () => {
+    it('should call userService.findOne', async () => {
+      const email = 'abc@example.com';
+      const password = 'password';
+      service.validateUser(email, password);
+      expect(mockUserService.findOne).toBeCalledWith(email);
+    });
+  });
+
+  describe('loginJwt', () => {
+    it('should call validateUser', async () => {
+      const dto = {
+        email: 'abc@example.com',
+        password: 'password',
+      };
+      const spy = jest.spyOn(service, 'validateUser');
+      try {
+        await service.loginJwt(dto);
+      } catch (e) {
+        // ignore invalid credentials
+      }
+      expect(spy).toBeCalledWith(dto.email, dto.password);
+    });
   });
 });
