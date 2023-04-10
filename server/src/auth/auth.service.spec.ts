67f2854e26a6f283b7f39327aee54e1812d6ab94
Irfanullah Jan <irfanullah.jan@outlook.com>
2023-04-10 14:43:30 -0400

    fixed failing test

--- server/src/auth/auth.service.spec.ts
+++ server/src/auth/auth.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockUserService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      providers: [AuthService, UserService, JwtService],
+    })
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
     service = module.get<AuthService>(AuthService);
   });
--- server/src/config.ts
+++ server/src/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-  public static readonly DATA_IMPORT_DISABLED = false;
+  public static readonly DATA_IMPORT_DISABLED = true;
   public static readonly DATA_IMPORT_BATCH_SIZE = 250;
   public static readonly DATA_IMPORT_CSV_SEPARATOR = ',';
   public static readonly MAX_AGE = 86400;
